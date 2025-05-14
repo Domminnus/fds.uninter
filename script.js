f966cabdbd0135caa64361d33e0d4e012e8ab3f1
Domminnus <diogo_henrique2016@outlook.com>
2025-05-14 18:56:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML = "Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo. (João 16:33)";
+    document.getElementById("mensagem").style.display = "block";
+}
